b7d16866d95843b669b4aadbd8eea688fec80bcf
Christopher <christopher.hall@sanger.ac.uk>
2017-04-04 15:01:38 +0100

    Simplifed script
    You no longer choose the parameters are load an example file at the start.  The CSV file now has the correct column names (compared to 1.2)

---
+++ Influx2CSV/Influx_Index_2_csv 1.3.R
@@ -0,0 +1,63 @@
+#Copyright (c) 2017 Genome Research Ltd.
+
+#InfluxIndex2CSV
+#v1.3 Apr 2017
+#R 3.3.3
+#Author : Christopher Hall, Wellcome Trust Sanger Institute, christopher.hall@sanger.ac.uk
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+#Extracts the index data from an Influx .fcs file and saves it as a .csv (Sortware v1.2.0.142 only, but you can test it on other versions if you like).  
+
+
+#Install required packages from bioconductor and CRAN.
+source("https://bioconductor.org/biocLite.R")
+biocLite("flowCore")
+library(flowCore)
+install.packages("dplyr")
+library(dplyr)
+#install.packages("fuzzyjoin")  
+
+#There is a bug in the current version of fuzzytools on CRAN (duplicates data).  Use the Github version for now
+install.packages("devtools") #you may already have this
+library(devtools)
+devtools::install_github("dgrtwo/fuzzyjoin")
+library(fuzzyjoin)
+
+setwd("")  #Point the script to your directory, Remember to swap \ with / on windows machines
+
+#This section loops through the index files and creates the .csv files
+files <- list.files(path=".", pattern=".fcs")
+for (fileName in files) {
+  flowfile<-read.FCS(fileName,alter.names=TRUE) #transformation remains on for influx index files
+  if (keyword(flowfile, "INDEXSORTPOSITIONS") %in% keyword(flowfile) ==TRUE){
+    
+    #Creates a datafrrame of the sorted events
+    ofintrest<-exprs(flowfile)
+    dataframe<-as.data.frame(ofintrest)
+    indexed<-dataframe[dataframe$'Sort.Result.Bits' >0,]
+    
+    #Creates a dataframe of the well positions
+    wellorder<-strsplit(keyword(flowfile,"INDEXSORTPOSITIONS")[[1]], ",")
+    WellID <- wellorder[[1]][seq(1, length(wellorder[[1]]), 3)]
+    x<-wellorder[[1]][seq(2, length(wellorder[[1]]), 3)]
+    y<-wellorder[[1]][seq(3, length(wellorder[[1]]), 3)]
+    Well_df<-data.frame(WellID,x,y)
+    Well_df[, 2] <- as.numeric(as.character( Well_df[, 2] ))
+    Well_df[, 3] <- as.numeric(as.character( Well_df[, 3] ))
+    colnames(Well_df) <- c("Well","Tray.X", "Tray.Y")
+    
+    #Sortware does not store the index positions correctly so we use thr dplyr and fuzzylogic packages to correct for this
+    indexdata<-difference_inner_join(Well_df,indexed,by=c("Tray.X", "Tray.Y"),max_dist = 3)
+    colnames(indexdata)<-c('Well','Tray X (kw)','Tray Y (kw)',colnames(read.FCS(fileName))) #Reading the fcs file again to get the column names slows the script down by 1/3.  To avoid this pre populate this with the col names.  Doing it this way makes the script more resistant to changes in the .fcs files
+    
+    #indexdata<-subset(indexdata,select=c("Well", "FSC", "SSC")) #use this line to choose your paramaters if you do not want all the columns
+    
+    #creates the csv file
+    filename=paste(substr(keyword(flowfile, "GUID"),1,(nchar(keyword(flowfile, "GUID"))-4))) #You could change 'GUID' to 'FIL' if you wish, i.e filename to actual saved name
+    write.csv(indexdata, file=paste(filename, "_index.csv", sep=""))}
+  }
+  
