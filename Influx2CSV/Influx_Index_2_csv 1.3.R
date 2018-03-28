5eabb8e7946851537aeb6958bfcc6dc1465447d5
Sanger Institute Flow Cytometry <ch15@sanger.ac.uk>
2018-03-28 15:43:00 +0100

    Shortened preamble

--- Influx2CSV/Influx_Index_2_csv 1.3.R
+++ Influx2CSV/Influx_Index_2_csv 1.3.R
@@ -1,14 +1,8 @@
-#Copyright (c) 2017 Genome Research Ltd.
-
 #InfluxIndex2CSV
 #v1.32 Jun 2017
 #R 3.3.3
 #Author : Christopher Hall, Wellcome Trust Sanger Institute, christopher.hall@sanger.ac.uk
-
-# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+#License : GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 
 #Extracts the index data from an Influx .fcs file and saves it as a .csv (Sortware v1.2.0.142 only, but you can test it on other versions if you like).  
 
