af0c35a4b57c03d5b354b19088eba95703d91dc2
Christopher <christopher.hall@sanger.ac.uk>
2018-08-22 10:35:57 +0100

    Moved into a function looping through the flowset
    And added loads of comments

--- Aria2csv.R
+++ Aria2csv.R
@@ -1,18 +1,20 @@
 #Christopher Hall, Wellcome Sanger Institute, ch15@sanger.ac.uk
-#R3.5.1
-#Bulk export of ARIA index information
-#Change setwd to the directory of your files
+#Bulk export of ARIA index information V2
+#Change setwd to the directory of your folder, now with added flowsetieness
 
+#load the required packages from BioConductor
 source("https://bioconductor.org/biocLite.R")
 biocLite("flowCore")
 library(flowCore)
-setwd("")
+setwd("C:/Users/ch15/Desktop/files")
 
-files <- list.files(path=".", pattern=".fcs")
-for (fileName in files) {
-  flowfile<-read.FCS(fileName)
-  editme<- data.frame(getIndexSort(flowfile))
-  editme$WellID<-chartr("01234567", "ABCDEFGH", editme$XLoc)
-  editme$WellID<-paste(editme$WellID,editme$YLoc +1, sep="")
-  write.csv(editme, file=paste(fileName, "_index.csv", sep=""))
-}
+files <- list.files(path=".", pattern=".fcs$") #list files in directory the $ means it MUST end with .fcs
+fs<-read.flowSet(files) #puts all the .fcs files into a flowset
+fsApply(fs,function(frame){ #loop through the flowset
+  comp <- keyword(frame)$SPILL #take the compensation matrix
+  new_frame <- compensate(frame,comp) #apply the comepensation matrix
+  editme<- data.frame(getIndexSort(new_frame)) #get the index data and convert to a data.frame to help with the WellID
+  editme$WellID<-chartr("01234567", "ABCDEFGH", editme$XLoc) #Repalce the XLoc numbers with well letters
+  editme$WellID<-paste(editme$WellID,editme$YLoc +1, sep="") #add one to each YLoc to match the plate designations
+  write.csv(editme, file=paste(keyword(new_frame,"GUID"), "_index.csv", sep="")) #write a csv using the GUID keyword as the filename
+})
