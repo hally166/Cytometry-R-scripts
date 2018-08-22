fa8fa1dec850eb1eb06fd3030caa6164b2402304
Christopher <christopher.hall@sanger.ac.uk>
2018-08-22 16:53:03 +0100

    added 384 well support
    used setNames to support 384 well plates

--- Aria2csv.R
+++ Aria2csv.R
@@ -1,5 +1,5 @@
 #Christopher Hall, Wellcome Sanger Institute, ch15@sanger.ac.uk
-#Bulk export of ARIA index information V2, now with added flowsetieness
+#Bulk export of ARIA index information V3, now with added flowsetieness and 384 well plate support
 #Change setwd to the directory of your folder
 
 #load the required packages from BioConductor
@@ -14,7 +14,10 @@ fsApply(fs,function(frame){ #loop through the flowset
   comp <- keyword(frame)$SPILL #take the compensation matrix
   new_frame <- compensate(frame,comp) #apply the comepensation matrix
   editme<- data.frame(getIndexSort(new_frame)) #get the index data and convert to a data.frame to help with the WellID
-  editme$WellID<-chartr("01234567", "ABCDEFGH", editme$XLoc) #Repalce the XLoc numbers with well letters
+  editme$XLoc<-chartr("0", "A", editme$XLoc)
+  platemap<-setNames(c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","N","P"), c("A","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"))
+  editme$WellID<-platemap[editme$XLoc]
+  editme$XLoc<-chartr("A", "0", editme$XLoc)
   editme$WellID<-paste(editme$WellID,editme$YLoc +1, sep="") #add one to each YLoc to match the plate designations
   write.csv(editme, file=paste(keyword(new_frame,"GUID"), "_index.csv", sep="")) #write a csv using the GUID keyword as the filename
 })
