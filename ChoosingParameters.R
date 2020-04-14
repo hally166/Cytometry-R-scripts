7904dd7a8738b11d6ac1e7c05b6698fe39f81e7b
Christopher <christopher.hall@embl.it>
2020-04-14 13:59:33 +0100

    Create ChoosingParameters.R

---
+++ ChoosingParameters.R
@@ -0,0 +1,34 @@
+#You need to input which parameters you need to analyse many times when dealing with FCS data in R.
+#Functions either require a list of names of parameters or an index or parameters (numbers).
+
+#Following are two functions that will help, especially for flowSets.
+
+#Create a list of names
+
+#Function - change the names within"" to exclude more (e.g. -W, FL1 etc..)
+autovect_verbose<- function(ff){
+  c<- data.frame(ff@parameters@data)
+  d<- grep("FSC|SSC|Time", c$name, invert = TRUE, value = TRUE)
+  return(unname(d))
+
+#Example use - transformation
+x<-autovect_verbose(fs_comp[[1]])
+tf<-estimateLogicle(fs_comp[[1]], channels =  x)
+fs_trans<-transform(fs_comp,tf)
+#Use in function
+x<-fsApply(fs, function(fr) autovect_verbose(fr))
+
+
+#Create an index
+
+#Function - change the names within"" to exclude more (e.g. -W, FL1 etc..)
+autovect<- function(ff){
+  c<- data.frame(ff@parameters@data)
+  d<- grep("FSC|SSC|Time", c$name, invert = TRUE)
+  return(as.vector(d))
+  
+#Example use - in flowClean
+fsclean<-fsApply(fs, function(fr) clean(fr, vectMarkers = autovect(fr),filePrefixWithDir = paste0(keyword(fr)$GUID," --QCOutput"), ext = "fcs", diagnostic = TRUE))
+#Standard use
+x<-autovect(fs_comp[[1]])
+x<-fsApply(fs, function(fr) autovect(fr))
