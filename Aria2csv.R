f8206e16d22a889b7e678a1e2aea57133795dafe
Christopher <christopher.hall@babraham.ac.uk>
2022-02-10 14:41:59 +0000

    removed compensation

--- Aria2csv.R
+++ Aria2csv.R
@@ -11,11 +11,11 @@ setwd("C:/Users/ch15/Desktop/files")
 
 #run these lines and csv files will be produced using the included compensation matrix for each file
 files <- list.files(path=".", pattern=".fcs$") #list files in directory the $ means it must end with .fcs
-fs<-read.flowSet(files) #puts all the .fcs files into a flowset
+fs<-read.flowSet(files, transformation = FALSE) #puts all the .fcs files into a flowset
 fsApply(fs,function(frame){ #loop through the flowset
-  comp <- keyword(frame)$SPILL #take the compensation matrix
+  comp <- keyword(frame)$SPILL #take the compensation matrix, remove 16+17 for uncompensated data
   new_frame <- compensate(frame,comp) #apply the comepensation matrix
-  editme<- data.frame(getIndexSort(new_frame)) #get the index data and convert to a data.frame to help with the WellID
+  editme<- data.frame(getIndexSort(new_frame)) #get the index data and convert to a data.frame to help with the WellID (new_frame -> frame if not compensating)
   editme$XLoc<-chartr("0", "A", editme$XLoc) #replace '0' with 'A' because setNames seems to not work with 0 with a really odd error
   editme$WellID<-setNames(c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","N","P"), c("A","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"))[editme$XLoc] #use setNames to rename the XLoc positions with well names 
   editme$XLoc<-chartr("A", "0", editme$XLoc) # #restore original XLoc '0' name
